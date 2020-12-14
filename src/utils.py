99700129af041393b303d03e59d2e5ac21da63d2
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-12-14 21:42:32 +0300

    udpated parser for udp server & cli

--- src/utils.py
+++ src/utils.py
@@ -28,16 +28,27 @@ def get_parser():
     parser.add_argument("-v", "--verbose", action="store_true", help="Show debug messages")
     subparsers = parser.add_subparsers(dest="subparser_name")
 
-    parser_server = subparsers.add_parser("server")
-    parser_server.add_argument("bind_host")
-    parser_server.add_argument("bind_ports", nargs='+', type=int)
-    parser_server.add_argument("--save_path", required=False, default=None)
+    parser_tcp_server = subparsers.add_parser("tcp-server")
+    parser_tcp_server.add_argument("bind_host")
+    parser_tcp_server.add_argument("bind_ports", nargs='+', type=int)
+    parser_tcp_server.add_argument("--save_path", required=False, default=None)
 
-    parser_client = subparsers.add_parser("client")
-    parser_client.add_argument("bind_ports", nargs='+', type=int)
-    parser_client.add_argument("server_host")
-    parser_client.add_argument("server_port", type=int)
-    parser_client.add_argument("file_path")
+    parser_tcp_client = subparsers.add_parser("tcp-client")
+    parser_tcp_client.add_argument("bind_ports", nargs='+', type=int)
+    parser_tcp_client.add_argument("server_host")
+    parser_tcp_client.add_argument("server_port", type=int)
+    parser_tcp_client.add_argument("file_path")
+
+    parser_udp_server = subparsers.add_parser("udp-server")
+    parser_udp_server.add_argument("bind_host")
+    parser_udp_server.add_argument("bind_ports", nargs='+', type=int)
+    parser_udp_server.add_argument("--save_path", required=False, default=None)
+
+    parser_udp_client = subparsers.add_parser("udp-client")
+    parser_udp_client.add_argument("bind_ports", nargs='+', type=int)
+    parser_udp_client.add_argument("server_host")
+    parser_udp_client.add_argument("server_port", type=int)
+    parser_udp_client.add_argument("file_path")
 
     return parser
 
