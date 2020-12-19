98f26bf1b36ef9d1567a64599cd2294c2e589356
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-12-19 22:44:03 +0300

    added server.py and client.py

---
+++ client.py
@@ -0,0 +1,32 @@
+import io
+import sys
+import threading
+
+from src.client.tcp import ZTransferTCPClient
+from src.client.udp import ZTransferUDPClient
+from src.utils import calc_sha3_512_checksum
+
+if __name__ == "__main__":
+    server_ip = sys.argv[1]
+    server_udp_port = int(sys.argv[2])
+    server_tcp_port = int(sys.argv[3])
+    client_udp_port = int(sys.argv[4])
+    client_tcp_port = int(sys.argv[5])
+
+    with open("transfer_file_TCP.txt", "rb") as fp:
+        file_stream = io.BytesIO(fp.read())
+        file_name = "transfer_file_TCP.txt"
+
+        client = ZTransferTCPClient(server_ip, server_tcp_port,
+            [client_tcp_port], file_name, file_stream, True)
+
+        client.initiate_transfer()
+
+    with open("transfer_file_UDP.txt", "rb") as fp:
+        file_stream = io.BytesIO(fp.read())
+        file_name = "transfer_file_UDP.txt"
+
+        client = ZTransferUDPClient(server_ip, server_udp_port,
+            [client_udp_port], file_name, file_stream, True)
+
+        client.initiate_transfer()
---
+++ server.py
@@ -0,0 +1,24 @@
+import sys
+import socket
+
+from src.server.tcp import ZTransferTCPServer
+from src.server.udp import ZTransferUDPServer
+
+hostname = socket.gethostname()
+self_ip = socket.gethostbyname(hostname)
+
+if __name__ == "__main__":
+    udp_port = int(sys.argv[1])
+    tcp_port = int(sys.argv[2])
+
+    server = ZTransferTCPServer(self_ip, [tcp_port], True)
+    server.listen_for_transfer()
+
+    with open("transfer_file_TCP.txt", "wb") as fp:
+        fp.write(server.recv_bytes_data)
+
+    server = ZTransferUDPServer(self_ip, [udp_port], True)
+    server.listen_for_transfer()
+
+    with open("transfer_file_UDP.txt", "wb") as fp:
+        fp.write(server.buffer_bytearray)
