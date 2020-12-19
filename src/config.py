f202f712efc9d54c134e96d8f91432709e166a4d
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-12-19 13:19:26 +0300

    lru cache hotfix

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from functools import lru_cache
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 CONF_FILE_PATH = os.path.normpath(os.path.join(SCRIPT_DIR, "config.json"))
 
-@lru_cache
+@lru_cache(maxsize=None)
 def get_config():
     with open(CONF_FILE_PATH, "r") as fp:
         return json.load(fp)
